4492ce70fe314f36f964f8c29425570a14471adf
@AlexP_FMS <alexis.payet@fms-ea.com>
2024-02-17 13:08:24 +0100

    update config for the name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'teeth_timer',
+  appName: 'Liste de course',
   webDir: 'www',
   bundledWebRuntime: false
 };
